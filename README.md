adf6349be48d78eb97bcccc072cb789b6a359cdc
lipeharakawa <felipeharakawa01@gmail.com>
2023-03-15 21:54:10 -0300

    adaptação

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-<h1 align="center"> Número Maior entre 3 Valores </h1>
+<h1 align="center">Lista Exercícios Disciplina Linguagem de Programação para a Web</h1>
 
 <p align="center">
-Programa desenvolvido em uma atividade durante o curso de Sistemas de Informação (UFSM/FW), na matéria de Linguagem de Programação para a Web.<br/>
+Programa desenvolvido em uma atividade durante o curso de Sistemas de Informação (UFSM/FW), na matéria de Linguagem de Programação para a Web e adaptado utilizando conceitos importantes de HTML, CSS e JavaScript.<br/>
 </p>
 
 <p align="center">
@@ -17,23 +17,33 @@ Programa desenvolvido em uma atividade durante o curso de Sistemas de Informaç�
 <br>
 
 <p align="center">
-  <img alt="Foto projeto Número Maior entre 3 Valores" src=".github/preview.jpg" width="100%">
+  <img alt="Foto página inicial" src=".github/preview.png" width="100%"> <br><br>
+  <img alt="Foto exercício 1" src=".github/exercicio-1.png" width="100%"> <br><br>
+  <img alt="Foto exercício 2" src=".github/exercicio-2.png" width="100%"> <br><br>
+  <img alt="Foto exercício 3" src=".github/exercicio-3.png" width="100%"> <br><br>
+  <img alt="Foto exercício 4" src=".github/exercicio-4.png" width="100%">
 </p>
 
 ## 🚀 Tecnologias
 
 Esse projeto foi desenvolvido com as seguintes tecnologias:
 
-- HTML e CSS
+- HTML
+- CSS
 - JavaScript
-- Git e Github
+- Git
+- Github
 - Visual Studio Code
 
 ## 💻 Projeto
 
-O projeto Número Maior entre 3 Valores é uma página web que recebe 3 números e escreve qual o maior deles.
+O Projeto desenvolvido contêm uma página principal com a descrição de cada exercício e um botão para acessar cada sistema, sendo: 
+- Exercício 1 é uma página web que recebe 4 valores e escreve a média deles. 
+- Exercício 2 é uma página web que recebe 3 números e escreve qual o maior deles. 
+- Exercício 3 é uma página web que recebe um valor de uma venda e a quantidade de parcelas (Máximo de 10 parcelas, sem juros). Ao final o programa exibe os valores de cada parcela, assim como o total pago. 
+- Exercício 4 é uma página web que recebe um valor de uma venda e a quantidade de parcelas (Máximo de 10 parcelas). Até 5 parcelas, não há incidência de juros. Se for escolhido de 6 a 10 parcelas, há uma incidência de 2% de juros ao mês, desde a 1ªa parcela. Ao final o programa exibe os valores de cada parcela, assim como o total pago. 
 
-- [ Visite o projeto online ](https://lipeharakawa.github.io/p_w_maior_3_numeros-html-css-js)
+- [ Visite o projeto online ](https://lipeharakawa.github.io/exercicios-html-css-js)
 
 ## ✅ Licença
 
@@ -41,4 +51,4 @@ Esse projeto está sob a licença MIT.
 
 ---
 
-Feito com 🧡 por Felipe Seidi Harakawa 👋.
+Feito com 🧡 por Felipe Seidi Harakawa.
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function calcular(){
+function calcular_maior(){
     var aux;
     var numero1 = parseInt(document.getElementById("numero1").value);
     var numero2 = parseInt(document.getElementById("numero2").value); 
@@ -27,9 +27,98 @@ function calcular(){
         maiorNumero.value = numero3;  
 }
 
-function limpar(){
+function limpar_maior(){
     document.getElementById("numero1").value = " ";
     document.getElementById("numero2").value = " ";
     document.getElementById("numero3").value = " ";
     document.getElementById("maiorNumero").value = " ";
+}
+
+function calcular_media(){
+    var numero1 = parseInt(document.getElementById("numero1").value);
+    var numero2 = parseInt(document.getElementById("numero2").value); 
+    var numero3 = parseInt(document.getElementById("numero3").value); 
+    var numero4 = parseInt(document.getElementById("numero4").value); 
+    var mediaConta = document.getElementById("mediaConta");
+
+    if (isNaN(numero1)) numero1=0;
+    if (isNaN(numero2)) numero2=0;
+    if (isNaN(numero3)) numero3=0;
+    if (isNaN(numero4)) numero4=0;
+
+    mediaConta.value = (numero1 + numero2 + numero3 + numero4) / 4;
+}
+
+function limpar_media(){
+    document.getElementById("numero1").value = " ";
+    document.getElementById("numero2").value = " ";
+    document.getElementById("numero3").value = " ";
+    document.getElementById("numero4").value = " ";
+    document.getElementById("mediaConta").value = " ";
+}
+
+function calcular_parcelas(){
+    valor = parseInt(document.getElementById("valor").value);
+    parcela = parseInt(document.getElementById("parcela").value);
+    var listaParcelas = document.getElementById("lista");
+
+    if(isNaN(valor)) v=0;
+    if(isNaN(parcela)) p=0;
+
+    if(parcela > 0 && parcela <= 10){
+
+        var valorParcela = valor / parcela;
+
+    for(i = 1; i <= parcela; i++){
+
+        listaParcelas.innerHTML = "Parcela " + i + " = R$ " + valorParcela.toFixed(2) + "<br>" + "Total = R$ " + valor.toFixed(2) + "<br>";
+    }
+    }else{
+        alert("❌ As parcelas devem ser de 1 a 10!");
+    }
+}
+
+function limpar_parcelas(){
+    document.getElementById("valor").value = " ";
+    document.getElementById("parcela").value = " ";
+}
+
+function calcular(){
+    valor = parseInt(document.getElementById("valor").value);
+    parcela = parseInt(document.getElementById("parcela").value);
+    var listaParcelas = document.getElementById("lista");
+    var soma;
+
+    if(isNaN(valor)) v=0;
+    if(isNaN(parcela)) p=0;
+
+    if(parcela > 0 && parcela <= 5){
+
+        var valorParcela = valor / parcela;
+
+    for(i = 1; i <= parcela; i++){
+
+        listaParcelas.innerHTML = "Parcela " + i + " = R$ " + valorParcela.toFixed(2) + "<br>" + "Total = R$ " + valor.toFixed(2) + "<br>";
+    }
+    }else if(parcela > 5 && parcela <= 10 ){
+    
+        var valorParcela = valor / parcela;
+        soma = 0;
+
+    for(i = 1; i <= parcela; i++){
+
+        valorParcela = valorParcela * 1.02;
+        soma = soma + valorParcela;
+
+        listaParcelas.innerHTML = "Parcela " + i + " = R$ " + valorParcela.toFixed(2) + "<br>" + "Total com Juros = R$ " + soma.toFixed(2) + "<br>";
+    }
+    }else {
+        alert("❌ As parcelas devem ser de 1 a 10!");
+    }
+}
+
+function limpar(){
+    document.getElementById("valor").value = " ";
+    document.getElementById("parcela").value = " ";
+    document.getElementById("parcela").value = " ";
 }
